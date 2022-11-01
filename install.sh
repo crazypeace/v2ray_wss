ebb7d8f543a4ccb9d47f3a9621decbac4b4be03a
crazypeace <665889+crazypeace@users.noreply.github.com>
2022-11-02 01:28:04 +0800

    v2ray_wss 提示信息

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ pause() {
 # 说明
 echo
 echo -e "$yellow此脚本仅兼容于Debian 10+系统. 如果你的系统不符合,请Ctrl+C退出脚本$none"
-echo -e "可以去 ${cyan}https://github.com/crazypeace/v2ray-wss${none} 查看脚本整体思路和关键命令, 以便针对你自己的系统做出调整."
+echo -e "可以去 ${cyan}https://github.com/crazypeace/v2ray_wss${none} 查看脚本整体思路和关键命令, 以便针对你自己的系统做出调整."
 echo -e "有问题加群 ${cyan}https://t.me/+D8aqonnCR3s1NTRl${none}"
 echo "本脚本支持带参数执行, 在参数中输入域名, 网络栈, UUID, path. 详见GitHub."
 echo "----------------------------------------------------------------"
