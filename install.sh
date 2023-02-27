0b908cdcafb127e4a95cf3d108d89c848b20e16e
crazypeace <665889+crazypeace@users.noreply.github.com>
2023-02-27 21:56:02 +0800

    去掉全部 git.io 方便配合 github proxy 脚本

--- install.sh
+++ install.sh
@@ -602,7 +602,8 @@ if [[ $netstack == "6" ]]; then
     pause
 
     # 安装 WARP IPv4
-    bash <(curl -L git.io/warp.sh) 4
+    # bash <(curl -L git.io/warp.sh) 4
+    bash <(curl -L https://raw.githubusercontent.com/P3TERX/warp.sh/main/warp.sh) 4
 
     # 重启 V2Ray
     echo
@@ -628,7 +629,8 @@ elif  [[ $netstack == "4" ]]; then
     pause
 
     # 安装 WARP IPv6
-    bash <(curl -L git.io/warp.sh) 6
+    # bash <(curl -L git.io/warp.sh) 6
+    bash <(curl -L https://raw.githubusercontent.com/P3TERX/warp.sh/main/warp.sh) 6
 
     # 重启 V2Ray
     echo
