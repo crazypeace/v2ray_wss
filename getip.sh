cb46a8ecc0d9136a895bfb0056672bdd611a969b
crazypeace <txican@gmail.com>
2024-05-19 17:58:43 +0800

    Update getip.sh

--- getip.sh
+++ getip.sh
@@ -38,23 +38,20 @@ curl -s https://www.cloudflare.com/cdn-cgi/trace | grep -oP "ip=\K.*$"
 
 # from TG "春风得意马蹄疾,一日看尽长安花" ID2075055328
 ip_address(){
-    InFaces=$(netstat -i | awk '{print $1}' | grep -E '^(eth|ens|eno|esp|enp|venet|vif)[0-9]+')
+    InFaces=($(netstat -i | awk '{print $1}' | grep -E '^(eth|ens|eno|esp|enp|venet|vif)[0-9]+'))
 
     for i in "${InFaces[@]}"; do
-        Public_IPv4=$(curl -s4 --interface "$i" ip.gs)
-        Public_IPv6=$(curl -s6 --interface "$i" ip.gs)
+        Public_IPv4=$(curl -s4 --interface "$i" ip.sb)
+        Public_IPv6=$(curl -s6 --interface "$i" ip.sb)
 
         # 检查是否获取到IP地址
-        if [[ -n "$Public_IPv4" ]]; then
+        if [[ -n "$Public_IPv4" || -n "$Public_IPv6" ]]; then
             ipv4_address="$Public_IPv4"
-        fi
-
-        if [[ -n "$Public_IPv6" ]]; then
             ipv6_address="$Public_IPv6"
+            break
         fi
     done
 }
-
 
 #
 curl -4L ifconfig.me
